807d1dfa95a044cead16d37c80f93e85238935b0
ahmed.negm <ahmed.negm@mts-egy.com>
2022-04-23 04:18:25 +0200

    finished perfromance page date filter except unit test

--- app/pages/performance-page.vue
+++ app/pages/performance-page.vue
@@ -1,20 +1,70 @@
 <template>
   <div class="c-dashboard">
     <div class="c-filters__container">
-      [Place filter component here]
+      <!-- [Place filter component here] -->
+      <date-range-filter-component 
+        :minDate="minDate" 
+        :maxDate="maxDate"
+        @filter-daterange="onFilterDaterange"
+      >
+      </date-range-filter-component>
     </div>
     <div class="c-dashboard__header">
-      <performance-chart-component />
+      <performance-chart-component :chartData="teamPerformanceData" />
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import PerformanceChartComponent from "../components/vue-components/performance-chart.vue";
+import DateRangeFilterComponent from '../components/vue-components/daterange-filter.vue';
 export default {
   name: "AboutPageComponent",
   components: {
     PerformanceChartComponent,
+    DateRangeFilterComponent
   },
+  data() {
+    return {
+      teamPerformanceData: [],
+      minDate: '',
+      maxDate: ''
+    }
+  },
+  async mounted() {
+    if(this.$store.getters.teamPerformanceChartPoints.length == 0) {
+      let teamPerformanceData = await axios.get('https://fe-task.getsandbox.com/performance')
+      if(teamPerformanceData) {
+        console.log('team performance data > ', teamPerformanceData)
+        this.teamPerformanceData = teamPerformanceData.data
+        this.minDate = this.getDateIsoFormat(this.teamPerformanceData[0].date_ms)
+        this.maxDate = this.getDateIsoFormat(this.teamPerformanceData[this.teamPerformanceData.length - 1].date_ms)
+        console.log('min date > ', this.minDate, this.maxDate)
+        this.$store.commit('setTeamPerformanceChartPoints', this.teamPerformanceData)
+        console.log('store chart points > ', this.$store.getters.teamPerformanceChartPoints)
+      }
+    } else {
+      this.teamPerformanceData = this.$store.getters.teamPerformanceChartPoints
+    }
+
+    
+  },
+  methods: {
+    onFilterDaterange(daterange) {
+      console.log('event range > ', daterange.startDate, daterange.endDate)
+      this.teamPerformanceData = this.getChartPointsInDaterange(daterange.startDate, daterange.endDate)
+    },
+    getChartPointsInDaterange(startDate, endDate) {
+      let filteredChartPoints = []
+      filteredChartPoints = this.$store.getters.teamPerformanceChartPoints.filter(ele => {
+        return ele.date_ms > new Date(startDate).getTime() && ele.date_ms <= new Date(endDate).getTime()
+      })
+      return filteredChartPoints
+    },
+    getDateIsoFormat(dateMs) {
+      return new Date(dateMs).toISOString().substr(0, 10)
+    }
+  }
 };
 </script>
