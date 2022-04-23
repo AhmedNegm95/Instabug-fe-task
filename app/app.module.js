81790f19b80f2eb6f6bce1e3f7b7b013c243c24f
ahmed.negm <ahmed.negm@mts-egy.com>
2022-04-23 05:02:23 +0200

    finished 404 page

--- app/app.module.js
+++ app/app.module.js
@@ -5,6 +5,7 @@ import store from './store.js';
 import PerformancePageComponent from './pages/performance-page.vue';
 import PerformanceChartComponent from './components/vue-components/performance-chart.vue';
 import EmployeesFilterComponent from './components/vue-components/employees-filter.vue';
+import NotFoundPageComponent from './pages/not-found-page.vue';
 
 angular.module('appModule', [
   'ui.router',
@@ -27,4 +28,8 @@ angular.module('appModule').directive('vPerformanceChart', (createVueComponent) 
 
 angular.module('appModule').directive('vEmployeesFilter', (createVueComponent) => {
   return createVueComponent(Vue.component('employeesFilterComponent', EmployeesFilterComponent));
+});
+
+angular.module('appModule').directive('vNotFoundPage', (createVueComponent) => {
+  return createVueComponent(Vue.component('notFoundPageComponent', NotFoundPageComponent));
 });
---
+++ app/pages/not-found-page.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="c-not-found">
+        <h1>Error 404</h1>
+    </div>
+</template>
